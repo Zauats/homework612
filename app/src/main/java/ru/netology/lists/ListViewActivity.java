d6f941d66e1f6474bf00080b6b49cff4e5ed90bb
Zauats <cfif12342002@yandex.ru>
2019-06-30 05:04:27 +0300

    vds

--- app/src/main/java/ru/netology/lists/ListViewActivity.java
+++ app/src/main/java/ru/netology/lists/ListViewActivity.java
@@ -1,6 +1,10 @@
 package ru.netology.lists;
 
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
@@ -8,8 +12,10 @@ import android.widget.SimpleAdapter;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -21,32 +27,67 @@ import java.util.Map;
 
 public class ListViewActivity extends AppCompatActivity {
 
+
+    List<Map<String, String>> simpleAdapterContent = new ArrayList<>();
+    SharedPreferences data;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // что-то там дефолтное
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        ListView list = findViewById(R.id.list);
+        final ListView list = findViewById(R.id.list);
 
-        List<Map<String, String>> values = prepareContent();
+        // добавляю данные в SharedPreference
+        data = getPreferences(MODE_PRIVATE);
+        if (!data.contains(getString(R.string.large_text))){
+            SharedPreferences.Editor myEditor = data.edit();
+            myEditor.putString("string", getString(R.string.large_text));
+            myEditor.apply();
+        }
 
-        BaseAdapter listContentAdapter = createAdapter(values);
+        simpleAdapterContent = getContent();
+        final BaseAdapter listContentAdapter = createAdapter(simpleAdapterContent);
         list.setAdapter(listContentAdapter);
+
+        // удаляю элемент
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Map<String, String> element = (Map<String, String>)listContentAdapter.getItem(position);
+                simpleAdapterContent.remove(element);
+                listContentAdapter.notifyDataSetChanged();
+            }
+        });
+
+        // Создаю обновление, но он почему-то не обновляется и ломается удаление
+        final SwipeRefreshLayout swipeLayout = findViewById(R.id.swipe);
+        swipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                simpleAdapterContent = getContent();
+                listContentAdapter.notifyDataSetChanged();
+                swipeLayout.setRefreshing(false);
+            }
+        });
+
     }
+
 
     @NonNull
     private BaseAdapter createAdapter(List<Map<String, String>> values) {
 
         int[] texts = {R.id.el1, R.id.el2};
-        return new SimpleAdapter(this, values, R.layout.list_item, new String[] {"Заголовок", "Подзаголовок"}, texts);
+        return new SimpleAdapter(this, values, R.layout.list_item, new String[] {"Подзаголовок", "Заголовок" }, texts);
     }
 
     @NonNull
-    private List<Map<String, String>> prepareContent() {
+    private List<Map<String, String>> getContent() {
         List<Map<String, String>> strings = new ArrayList<Map<String, String>>();
 
-        String[] arrayContent = getString(R.string.large_text).split("\n\n");
+        String[] arrayContent = data.getString("string", "").split("\n\n");
         for (int i = 0; i < arrayContent.length - 1; i++) {
             Map<String, String> content = new HashMap<String, String>();
             content.put("Заголовок", arrayContent[i]);
